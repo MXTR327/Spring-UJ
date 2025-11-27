a99632e21391e82be11698a92305f89a8aa308ac
MXTR327 <maxramirez327@gmail.com>
2025-11-26 19:46:51 -0500

    dote: Inicializar la aplicación Zona Fit con Spring Boot y Swing UI - Se añadió Maven Wrapper para la gestión de proyectos. - Creó pom.xml con dependencias de Spring Boot incluyendo JPA y MariaDB. - Implementó la clase principal de aplicación ZonaFitSwing para lanzar la interfaz Swing. - Diseñó la interfaz gráfica ZonaFitForma con componentes para la gestión de clientes. - Desarrolló la clase modelo Cliente para la representación de entidades JPA. - Creó la interfaz IClienteRepositorio para operaciones de bases de datos. - Implementó ClienteServicio para la lógica de negocio y la gestión de clientes. - Se añadió la interfaz IClienteServicio para la abstracción de servicios. - Propiedades configuradas de la aplicación para la conexión a la base de datos. - Configurar la configuración de registro con Logback. - Se añadió la prueba unitaria para la carga del contexto de la aplicación.

---
+++ 40-SpringBoot.JSF.PrimeFaces.Maven.Java-ZonaFit/src/test/java/gm/zona_fit/ZonaFitApplicationTests.java
@@ -0,0 +1,15 @@
+package gm.zona_fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonaFitApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
